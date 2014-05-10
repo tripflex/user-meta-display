be62d85a5c84fb7e47671a60d25a62687eebca8f
Myles McNamara <myles@smyl.es>
2014-05-10 13:45:34 -0400

    pass user_id

--- includes/field-user_list_dropdown.php
+++ includes/field-user_list_dropdown.php
@@ -1,3 +1,22 @@
 <div id="umd_user_list_dropdown">
-<?php wp_dropdown_users(array('show_option_none' => 'Select User Display Name', 'class' => 'user-meta-display-user-dropdown')); ?>
+<?php
+	$user_args = array(
+		'show_option_none' => 'Select User Display Name',
+		'class' => 'user-meta-display-user-dropdown'
+	);
+
+	// User ID specified through GET
+	if(intval($_GET['user_id'])):
+		$user_args['selected'] = intval($_GET['user_id']);
+?>
+		<script>
+			jQuery(function($){
+				$('#user').click();
+			});
+		</script>
+<?php
+	endif;
+
+	wp_dropdown_users($user_args);
+?>
 </div>
--- includes/functions-user_meta_display.php
+++ includes/functions-user_meta_display.php
@@ -4,8 +4,8 @@ add_action( 'wp_ajax_umd_change_user_list_dropdown', 'umd_change_user_list_dropd
 add_filter( 'user_row_actions', 'umd_filter_user_row_actions', 10, 2 );
 
 function umd_filter_user_row_actions( array $actions, WP_User $user ) {
-	$link = admin_url( 'users.php?page=user_meta_display' );
-	$actions['user_meta'] = '<a href="' . $link . '">' . __( 'View Meta', 'user-meta-display' ) . '</a>';
+	$link = admin_url( 'users.php?page=user_meta_display&user_id=' . $user->ID );
+	$actions['user_meta'] = '<a href="' . $link . '">' . __( 'Meta', 'user-meta-display' ) . '</a>';
 
 	return $actions;
 }
